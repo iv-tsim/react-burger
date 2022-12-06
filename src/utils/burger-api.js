01a8ef4cb1e9485f9482236541c07fcb1442cddd
Ivan <iv-tsim@yandex.ru>
2022-12-06 14:35:49 +0300

    Промежуточная проверка

--- src/utils/burger-api.js
+++ src/utils/burger-api.js
@@ -1,5 +1,6 @@
 export const BASE_API_URL = 'https://norma.nomoreparties.space/api';
 export const INGREDIENTS_URL = `${BASE_API_URL}/ingredients`;
+export const ORDER_DETAILS_URL = `${BASE_API_URL}/orders`;
 
 const checkReponse = (res) => {
 	return res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
@@ -10,6 +11,23 @@ export const getIngredientsData = () => {
 		.then((response) => checkReponse(response))
 		.then((response) => {
 			return response.data;
+		})
+		.catch((error) => {
+			throw new Error(error);
+		});
+};
+
+export const getOrderDetails = (data) => {
+	return fetch(ORDER_DETAILS_URL, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json;charset=utf-8',
+		},
+		body: JSON.stringify(data),
+	})
+		.then((response) => checkReponse(response))
+		.then((response) => {
+			return response;
 		})
 		.catch((error) => {
 			throw new Error(error);
